8548a25dec677f98125683ef3c86eabbd0bc0d08
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-05-19 17:23:13 +0600

    update code added

--- index.js
+++ index.js
@@ -2,19 +2,24 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const fs = require('fs');
+// const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
+// const corsConfig = {
+//     origin: '',
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE']
+// }
+// app.use(cors(corsConfig))
+// app.options("", cors(corsConfig))
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Assignment-11-server is running');
-});
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vweq3se.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vweq3se.mongodb.net/your-database-name?retryWrites=true&w=majority`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,8 +33,8 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server (optional starting in v4.7)
-        await client.connect();
-        app.use(express.static('public'));
+        // client.connect();
+        // app.use(express.static('public'));
 
         // ...
 
@@ -43,6 +48,19 @@ async function run() {
             }
         });
 
+
+        app.get('/toys', async (req, res) => {
+            try {
+                const toysCollection = client.db('toys').collection('toys');
+                const toysData = await toysCollection.find().toArray();
+                res.json(toysData);
+            } catch (error) {
+                console.error(error);
+                res.status(500).json({ error: 'Internal Server Error' });
+            }
+        });
+
+
         // ...
 
 
@@ -51,12 +69,14 @@ async function run() {
         console.log('Pinged your deployment. You successfully connected to MongoDB!');
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 
 run().catch(console.dir);
-
+app.get('/', (req, res) => {
+    res.send('Assignment-11-server is running');
+});
 app.listen(port, () => {
     console.log(`Server is running on PORT: ${port}`);
 });
