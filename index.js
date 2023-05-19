f8958269c0d78fc0708474802a690974f10f862c
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-05-19 09:52:46 +0600

    toys.json file created

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send('Assignment-11-server is running')
-})
-
-
+    res.send('Assignment-11-server is running');
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vweq3se.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -19,24 +22,41 @@ const client = new MongoClient(uri, {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
-    }
+    },
 });
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
+        // Connect the client to the server (optional starting in v4.7)
         await client.connect();
+        app.use(express.static('public'));
+
+        // ...
+
+        app.get('/toys', (req, res) => {
+            try {
+                const toysData = fs.readFileSync('./data/toys.json', 'utf8');
+                res.json(JSON.parse(toysData));
+            } catch (error) {
+                console.error(error);
+                res.status(500).json({ error: 'Internal Server Error' });
+            }
+        });
+
+        // ...
+
+
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        await client.db('admin').command({ ping: 1 });
+        console.log('Pinged your deployment. You successfully connected to MongoDB!');
     } finally {
         // Ensures that the client will close when you finish/error
         await client.close();
     }
 }
+
 run().catch(console.dir);
 
-
 app.listen(port, () => {
-    console.log(`Server is running on PORT: ${port}`)
-})
+    console.log(`Server is running on PORT: ${port}`);
+});
