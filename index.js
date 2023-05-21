9b7a429da024546d491d1e70f4e63eccaa13557e
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-05-21 17:29:54 +0600

    delete api added

--- index.js
+++ index.js
@@ -6,22 +6,11 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// middleware
-// const corsConfig = {
-//     origin: '',
-//     credentials: true,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE']
-// }
-// app.use(cors(corsConfig))
-// app.options("", cors(corsConfig))
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vweq3se.mongodb.net/your-database-name?retryWrites=true&w=majority`;
 
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -32,15 +21,12 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
+
         const toysCollection = client.db('toyCars').collection('toys');
-        const newToysCollection = client.db('toyCars').collection('toys');
-
-
 
         app.get('/toys', async (req, res) => {
             try {
-
                 const toysData = await toysCollection.find().toArray();
                 res.json(toysData);
             } catch (error) {
@@ -49,46 +35,73 @@ async function run() {
             }
         });
 
-
-        app.get('/toy/:id', async (req, res) => {
-
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-            const toy = await toysCollection.findOne(query)
-            res.send(toy)
-
+        app.get('/uploads', async (req, res) => {
+            try {
+                const { email } = req.query;
+                const toysData = await toysCollection.find({ email: email }).toArray();
+                res.json(toysData);
+            } catch (error) {
+                console.error(error);
+                res.status(500).json({ error: 'Internal Server Error' });
+            }
         });
 
+        app.get('/toy/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const toy = await toysCollection.findOne(query);
+            res.send(toy);
+        });
 
         app.post('/api/toys', async (req, res) => {
-            const { pictureUrl, name, sellerName, sellerEmail, category, price, rating, availableQuantity, description } = req.body;
-            const toyData = {
-                pictureUrl,
-                name,
-                sellerName,
-                sellerEmail,
+            const {
+                image,
+                toy_name,
+                seller,
+                email,
                 category,
                 price,
                 rating,
-                availableQuantity,
+                available_quantity,
+                description,
+            } = req.body;
+
+            const toyData = {
+                image,
+                toy_name,
+                seller,
+                email,
+                category,
+                price,
+                rating,
+                available_quantity,
                 description,
             };
 
-            newToysCollection.insertOne(toyData, (err) => {
-                if (err) {
-                    console.error(err);
-                    res.status(500).send('Error saving the toy');
+            const result = await toysCollection.insertOne(toyData);
+            res.send(result);
+        });
+
+        app.delete('/api/toys/:id', async (req, res) => {
+            try {
+                const id = req.params.id;
+                const query = { _id: new ObjectId(id) };
+
+                const result = await toysCollection.deleteOne(query);
+
+                if (result.deletedCount === 1) {
+                    res.json({ message: 'Toy deleted successfully' });
                 } else {
-                    res.status(200).send('Toy saved successfully');
+                    res.status(404).json({ error: 'Toy not found' });
                 }
-            });
+            } catch (error) {
+                console.error(error);
+                res.status(500).json({ error: 'Internal Server Error' });
+            }
         });
 
 
-
-        // Send a ping to confirm a successful connection
-        await client.db('admin').command({ ping: 1 });
-        console.log('Pinged your deployment. You successfully connected to MongoDB!');
+        console.log('Connected to MongoDB!');
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
@@ -96,9 +109,11 @@ async function run() {
 }
 
 run().catch(console.dir);
+
 app.get('/', (req, res) => {
     res.send('Assignment-11-server is running');
 });
+
 app.listen(port, () => {
     console.log(`Server is running on PORT: ${port}`);
 });
